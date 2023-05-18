54771a21f1572e2ef37d1d7f1934bce78f4f4b49
samiyeiyene23 <yenerjose@outlook.es>
2023-05-17 19:26:41 -0500

    cambio de archivo php

--- app.php
+++ app.php
@@ -2,6 +2,8 @@
 $firstNumber = 2;
 $secondNumber = 4;
     echo "firstNumber + secondNumber";
-
+    echo "firstNumber - secondNumber";
+    echo "firstNumber * secondNumber";
+    echo "firstNumber / secondNumber";
 ?>
 
--- idex.php
+++ idex.php
@@ -1,5 +1,6 @@
 <?php
     echo "Bienvenido"
+    echo "Este es mi primer codigo"
     
 ?>
 
